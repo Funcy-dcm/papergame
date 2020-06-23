92151b63aa46a2a3fff13a347505904f1837ec44
Shilyaev Egor <egor.shilyaev@gmail.com>
2020-06-23 10:15:52 +0500

    change: Обновлён перевод

--- translations/papergame_ru.ts
+++ translations/papergame_ru.ts
@@ -2,14 +2,62 @@
 <!DOCTYPE TS>
 <TS version="2.1" language="ru">
 <context>
+    <name>HelpPage</name>
+    <message>
+        <location filename="../resources/qml/HelpPage.qml" line="76"/>
+        <source>Player Aids</source>
+        <translation>Помощь для игроков</translation>
+    </message>
+    <message>
+        <location filename="../resources/qml/HelpPage.qml" line="77"/>
+        <source>Shortcuts</source>
+        <translation>Быстрые клавиши</translation>
+    </message>
+    <message>
+        <location filename="../resources/qml/HelpPage.qml" line="78"/>
+        <source>Flip over cards</source>
+        <translation>Перевернуть карты</translation>
+    </message>
+    <message>
+        <location filename="../resources/qml/HelpPage.qml" line="79"/>
+        <source>Return cards</source>
+        <translation>Вернуть карты</translation>
+    </message>
+    <message>
+        <location filename="../resources/qml/HelpPage.qml" line="80"/>
+        <source>Flip over City Plan 1-3</source>
+        <translation>Перевернуть план города 1-3</translation>
+    </message>
+    <message>
+        <location filename="../resources/qml/HelpPage.qml" line="81"/>
+        <source>New game</source>
+        <translation>Новая игра</translation>
+    </message>
+    <message>
+        <location filename="../resources/qml/HelpPage.qml" line="82"/>
+        <source>Shuffle City Plans</source>
+        <translation>Перемешать планы города</translation>
+    </message>
+    <message>
+        <location filename="../resources/qml/HelpPage.qml" line="83"/>
+        <source>Shuffle cards</source>
+        <translation>Перемешать карты</translation>
+    </message>
+    <message>
+        <location filename="../resources/qml/HelpPage.qml" line="84"/>
+        <source>Full Screen</source>
+        <translation>Полноэкранный режим</translation>
+    </message>
+</context>
+<context>
     <name>MainWindow</name>
     <message>
-        <location filename="../resources/qml/MainWindow.qml" line="115"/>
+        <location filename="../resources/qml/MainWindow.qml" line="118"/>
         <source>Cards</source>
         <translation>Карты</translation>
     </message>
     <message>
-        <location filename="../resources/qml/MainWindow.qml" line="117"/>
+        <location filename="../resources/qml/MainWindow.qml" line="120"/>
         <location filename="../resources/qml/MainWindow.qml" line="265"/>
         <source>Help</source>
         <translation>Помощь</translation>
@@ -26,43 +74,66 @@
     </message>
     <message>
         <location filename="../resources/qml/MainWindow.qml" line="266"/>
-        <source>Rules...</source>
+        <source>Rulebook...</source>
         <translation>Правила...</translation>
     </message>
     <message>
         <location filename="../resources/qml/MainWindow.qml" line="267"/>
-        <source>Sheet...</source>
-        <translation>Листок...</translation>
+        <source>Player sheet...</source>
+        <translation>Лист игрока (печать)...</translation>
     </message>
     <message>
         <location filename="../resources/qml/MainWindow.qml" line="268"/>
-        <source>Mix targets</source>
-        <translation>Перемешать цели</translation>
+        <source>New game</source>
+        <translation>Новая игра</translation>
     </message>
     <message>
         <location filename="../resources/qml/MainWindow.qml" line="269"/>
-        <source>Mix cards</source>
-        <translation>Перемешать карты</translation>
+        <source>Shuffle City Plans</source>
+        <translation>Перемешать планы города</translation>
     </message>
     <message>
         <location filename="../resources/qml/MainWindow.qml" line="270"/>
+        <source>Shuffle cards</source>
+        <translation>Перемешать карты</translation>
+    </message>
+    <message>
+        <location filename="../resources/qml/MainWindow.qml" line="273"/>
+        <source>Flip over cards</source>
+        <translation>Перевернуть карты</translation>
+    </message>
+    <message>
+        <location filename="../resources/qml/MainWindow.qml" line="271"/>
         <source>Expert mode</source>
         <translation>Экспертный режим</translation>
     </message>
     <message>
-        <location filename="../resources/qml/MainWindow.qml" line="271"/>
+        <location filename="../resources/qml/MainWindow.qml" line="272"/>
         <source>Return cards</source>
         <translation>Вернуть карты</translation>
     </message>
     <message>
-        <location filename="../resources/qml/MainWindow.qml" line="272"/>
-        <source>Flip cards</source>
-        <translation>Перевернуть карты</translation>
-    </message>
-    <message>
-        <location filename="../resources/qml/MainWindow.qml" line="273"/>
+        <location filename="../resources/qml/MainWindow.qml" line="274"/>
         <source>Exit</source>
         <translation>Выход</translation>
+    </message>
+</context>
+<context>
+    <name>NameDialog</name>
+    <message>
+        <location filename="../resources/qml/NameDialog.qml" line="30"/>
+        <source>City name</source>
+        <translation>Название города</translation>
+    </message>
+    <message>
+        <location filename="../resources/qml/NameDialog.qml" line="46"/>
+        <source>Ok</source>
+        <translation>Ok</translation>
+    </message>
+    <message>
+        <location filename="../resources/qml/NameDialog.qml" line="53"/>
+        <source>Cancel</source>
+        <translation>Отмена</translation>
     </message>
 </context>
 </TS>
